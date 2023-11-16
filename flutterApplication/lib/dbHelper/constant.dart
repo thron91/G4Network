63f5fad38a8f19b7245f36cb303f3474197f67aa
Jeancy MUPUANGA LUMBANZILA <36196471+Skyrix91@users.noreply.github.com>
2023-11-16 14:00:45 +0100

    Update constant.dart

--- flutterApplication/lib/dbHelper/constant.dart
+++ flutterApplication/lib/dbHelper/constant.dart
@@ -1,5 +1,5 @@
 // ignore_for_file: constant_identifier_names
 
 const MONGO_URL =
-    "mongodb+srv://ecoservice93projet:6XWObdRP07m1HsPZ@g4.p3rtvnv.mongodb.net/mydatabase?retryWrites=true&w=majority";
+    "";
 const USER_COLLECTION = "Students";
